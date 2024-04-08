37de3b876f32fcd373be308570282c909da95874
Bhupesh-Chaubey <bhupeshchaubey.07@gmail.com>
2024-04-08 16:11:01 +0530

    Learn Variables

--- JavaScript/Lecture 1.js
+++ JavaScript/Lecture 1.js
@@ -1,1 +1,18 @@
-console.log("Hello World")
+// Printing Hello World
+console.log("Hello World");
+
+// Learning Variables
+age = "Your age is 17";
+console.log(age);
+
+name = "Bhupesh Chaubey";
+console.log(name);
+
+x = null;
+console.log(x);
+
+y = undefined;
+console.log(y);
+
+isFollow = false;
+console.log(isFollow)
