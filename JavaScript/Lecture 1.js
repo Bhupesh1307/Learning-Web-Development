ebae1733a9e02116e144d9cb1a7dfdd59cbd1b54
Bhupesh-Chaubey <bhupeshchaubey.07@gmail.com>
2024-04-11 21:36:31 +0530

    Learn Data Types

--- JavaScript/Lecture 1.js
+++ JavaScript/Lecture 1.js
@@ -16,3 +16,31 @@ console.log(y);
 
 isFollow = false;
 console.log(isFollow);
+
+//Data types
+
+// Premetive Data types
+let fullName = "Tony Stark"; // String
+
+let num = 18; // Number
+
+let isFollowing = true; // Boolean
+
+let none = null; // Null
+
+let notDefinded = undefined; // Undefined
+
+let bINT = ("123"); // BigInt
+
+let symb = ("Hello!"); // Symbol
+
+// Non-Premetive Data types
+const student = {
+    fullName : "Rahul Sarkar",
+    age : 18,
+    marks : 480,
+    isPass : true
+};
+
+console.log(student);
+console.log(typeof(student));
