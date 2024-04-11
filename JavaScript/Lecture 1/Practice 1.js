6e0e206bfd4d2ff90abc7b11e713431374817af0
Bhupesh-Chaubey <bhupeshchaubey.07@gmail.com>
2024-04-11 22:10:17 +0530

    Complete Practice Set 1

---
+++ JavaScript/Lecture 1/Practice 1.js
@@ -0,0 +1,18 @@
+const product = {
+    title : "Ball Pen",
+    rating : 4,
+    offer : 5,
+    price : 220
+};
+
+console.log(product);
+
+const profile = {
+    userName : "shradhakhapra",
+    followers : 56900,
+    following : 4,
+    isFollow : false,
+    posts : 195
+};
+
+console.log(profile);
