7b8726fe848026fa93c3608377e6de7235956c89
Bhupesh-Chaubey <bhupeshchaubey.07@gmail.com>
2024-04-12 19:28:48 +0530

    JavaScript Lecture 2

---
+++ JavaScript Lecture 2/Lecture 2.js
@@ -0,0 +1,16 @@
+// Comments in JavaScript
+
+// This is a Single Line Comment
+/* This
+        is
+            a
+                Multiline
+                            Comment*/
+
+// Operators in JavaScript
+
+// Arithmatic Operators
+let a = 5;
+let b = 2;
+let sum = a + b;
+console.log("5 + 2 =", sum);
