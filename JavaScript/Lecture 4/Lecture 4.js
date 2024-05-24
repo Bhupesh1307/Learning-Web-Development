6549e7f03fce85d4e8f5d29b05a72aef4e21f230
Bhupesh-Chaubey <bhupeshchaubey.07@gmail.com>
2024-05-24 19:43:33 +0530

    Learn Array Methods

--- JavaScript/Lecture 4/Lecture 4.js
+++ JavaScript/Lecture 4/Lecture 4.js
@@ -31,3 +31,71 @@ while (j < marks.length) {
 for (let items of superHeros){
     console.log(items);
 }
+
+// Arrays Methods in JavaScript
+// These methods do changes to the original arrays
+// Push(): add to end
+// Pop(): delete from end & return
+// Unshift(): add a new item in the start of the array
+// Shift(): deletes one element in the start of the array
+// Splice()
+
+// These methods do changes and make it a new array
+// toString(): converts array to string
+// Concat(): joins multiple arrays & return results
+// Slice()
+
+
+// array.push()
+let foodItems = ["potato", "tomato", "grapes", "mango", "burger"];
+console.log(foodItems);
+foodItems.push("lays");     // Adds lays to the array "foodItems"
+console.log(foodItems);
+
+// array.pop()
+let deletedItem = foodItems.pop(); // It deletes one element from the last of the array
+console.log(foodItems);
+console.log("Deleted", deletedItem);
+
+// toString()
+let foodString = foodItems.toString(); // Converts the array to the string
+console.log(foodString);
+
+let marvelHeros = ["IronMan", "SpiderMan", "Thor", "CaptainAmerica"];
+let dcHeros = ["SuperMan", "Batman", "WonderWoman"];
+let indianHeros = ["Krish", "Shaktiman"];
+let Heroes = marvelHeros.concat(dcHeros, indianHeros); // Combines all the given arrays
+console.log(marvelHeros);
+console.log(dcHeros);
+console.log(indianHeros);
+console.log(Heroes);
+
+// unshift()
+marvelHeros.unshift("Hulk"); // Adds Hulk in the start of the array
+console.log(marvelHeros);
+
+// shift()
+let removed = marvelHeros.shift() // Deletes one element in the start of the array
+console.log(marvelHeros);
+console.log("Removed", removed);
+
+// Slice()
+console.log(marvelHeros.slice(0,1)); // Takes a part of array from index 0 to 1 but 1 is not inclusive
+
+// Splice()
+let num = [1, 2, 3, 4, 5, 6, 7];
+console.log(num);
+num.splice(1, 2, 101, 102); // Splice can delete, add and replace itmes in an array
+console.log(num);           // arrat.splice(startIdx, delCount, newElement)
+
+// Add Element
+num.splice(3, 0, 420);
+console.log(num);
+
+// Delete Element
+num.splice(5, 1);
+console.log(num);
+
+// Replace Element
+num.splice(4, 1, 1307);
+console.log(num);
