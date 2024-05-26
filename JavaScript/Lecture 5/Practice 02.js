ad1b99f839b0429f5ec63f9bcd65f50cb6fefd28
Bhupesh-Chaubey <bhupeshchaubey.07@gmail.com>
2024-05-26 21:47:29 +0530

    Complete Practice 02

---
+++ JavaScript/Lecture 5/Practice 02.js
@@ -0,0 +1,7 @@
+/**
+    Q.1> For a given array of numbers, pringt the square of each valuse using forEach loop.
+*/
+let num = [1, 2, 3, 4, 5];
+num.forEach((val) => {
+    console.log(val ** 2);
+})
