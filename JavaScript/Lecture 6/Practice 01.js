3b62fe2e1538520442300e2ff0f021a0ad2ec311
Bhupesh-Chaubey <bhupeshchaubey.07@gmail.com>
2024-06-24 22:09:16 +0530

    Solve Preactice Sets

---
+++ JavaScript/Lecture 6/Practice 01.js
@@ -0,0 +1,7 @@
+let h2 = document.querySelector("h2");
+h2.innerText += " from Apna College";
+
+let box = document.querySelectorAll(".box");
+box[0].innerText = "text1";
+box[1].innerText = "text2";
+box[2].innerText = "text3";
