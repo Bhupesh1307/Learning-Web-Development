1bf7d1ec109afe3e9b6d0aec0bba25902e42ae2d
Bhupesh <bhupeshchaubey.07@gmail.com>
2025-09-29 22:29:05 +0530

    feat: add favicon.ico, add seekbar, add searchbox hover/focus effects, build logic in JS to play songs from the library

--- Projects/Spotify Clone/src/script.js
+++ Projects/Spotify Clone/src/script.js
@@ -1,6 +1,6 @@
 async function fetchSongs() {
   let a = await fetch(
-    "http://127.0.0.1:5500/Projects/Spotify%20Clone/src/songs"
+    "http://127.0.0.1:5500/Projects/Spotify%20Clone/src/songs/"
   );
   let response = await a.text();
   let div = document.createElement("div");
@@ -10,33 +10,57 @@ async function fetchSongs() {
   for (let index = 0; index < links.length; index++) {
     let element = links[index];
     if (element.href.endsWith(".mp3")) {
-      songs.push(element.href.split("/songs/")[1]);
+      let filename = element.href.split("src/songs/")[1];
+      songs.push("src/songs/" + filename);
     }
   }
   return songs;
 }
 
-async function main() {
-  let songs = await fetchSongs();
-  console.log(songs);
+let currentSong = new Audio()
 
+function musicPlay(track) {
+  track = track.replaceAll("&amp;", "%26")
+  currentSong.src = "src/songs/" + track;
+  currentSong.play();
+}
+
+async function main() {
+  // Get the list of all the songs
+  let songs = await fetchSongs();
+
+  // Show all the songs in the list
   let songsList = document
     .querySelector(".songsList")
     .getElementsByTagName("ul")[0];
   for (const song of songs) {
-    songsList.innerHTML = songsList.innerHTML + `<li>
+    let displayName = decodeURIComponent(song.split("/").pop());
+    songsList.innerHTML =
+      songsList.innerHTML +
+      `<li>
                 <div class="musicIconContainer flex justify-center align-center">
                   <img src="src/img/music-note-04-stroke-rounded.svg" alt="Music Icon" class="musicIcon invert">
                 </div>
-                ${song.replaceAll("%20", " ").replaceAll("%26", "&")} 
+                <div class="songName">
+                  ${displayName} 
+                </div>
                 <div class="play flex justify-center align-center">
                   <img src="src/img/play.svg" alt="Play Song" class ="invert playBtn">
                 </div>
               </li>`;
   }
 
-  var firstSong = new Audio(songs[0]);
-  // firstSong.play();
+  // Attach an even listenener to each song
+  Array.from(
+    document.querySelector(".songsList").getElementsByTagName("li")
+  ).forEach((e) => {
+    console.log(e);
+    e.addEventListener("click", () => {
+      let song = e.querySelector(".songName").innerHTML.trim();
+      console.log("Playing " + song)
+      musicPlay(song);
+    });
+  });
 }
 
 main();
